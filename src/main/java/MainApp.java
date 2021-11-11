f9ca296ee613d59c663af803bd7ac9627f57faaa
Karimova Zilya <zilja081295@mail.ru>
2021-11-11 10:22:52 +0500

    add homework of 25th module

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,6 +1,10 @@
+import java.io.IOException;
+
 public class MainApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+// =========================Homework module 24==============================
         Student student = new Student.Builder()
                 .fullName("Анатолий Степанов")
                 .universityId("1236")
@@ -17,5 +21,9 @@ public class MainApp {
                 .build();
 
         System.out.printf("Студент: %s\nУниверситет: %s%n", student, university);
+
+// =========================Homework module 25==============================
+        System.out.println(ReadExcelFile.readStudents("C:\\Users\\Admin\\IdeaProjects\\HMW_module24\\src\\main\\resources\\universityInfo.xlsx"));
+        System.out.println(ReadExcelFile.readUniversities("C:\\Users\\Admin\\IdeaProjects\\HMW_module24\\src\\main\\resources\\universityInfo.xlsx"));
     }
 }
---
+++ src/main/java/ReadExcelFile.java
@@ -0,0 +1,87 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ReadExcelFile {
+
+    private ReadExcelFile() {
+    }
+
+    public static List<Student> readStudents(String path) throws IOException {
+        List<Student> students = new ArrayList<>();
+        File file = new File("C:\\Users\\Admin\\IdeaProjects\\HMW_module24\\src\\main\\resources\\universityInfo.xlsx");
+        FileInputStream fis = new FileInputStream(file);
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+        XSSFSheet sheet = wb.getSheetAt(0);
+        Iterator<Row> itr = sheet.iterator();
+        itr.next();
+        while (itr.hasNext()) {
+            Row row = itr.next();
+            Iterator<Cell> cellIterator = row.cellIterator();   //iterating over each column
+            Student student = new Student();
+            Cell cell = null;
+            while (cellIterator.hasNext()) {
+                cell = cellIterator.next();
+                if (cell.getCellType() == CellType.STRING) {
+                    if (cell.getColumnIndex() == 0) {
+                        student.setUniversityId(cell.getStringCellValue());
+                    } else if (cell.getColumnIndex() == 1) {
+                        student.setFullName(cell.getStringCellValue());
+                    }
+                } else if (cell.getCellType() == CellType.NUMERIC) {
+                    if (cell.getColumnIndex() == 2) {
+                        student.setCurrentCourseNumber((int) cell.getNumericCellValue());
+                    } else if (cell.getColumnIndex() == 3) {
+                        student.setAvgExamScore((float) cell.getNumericCellValue());
+                    }
+                }
+            }
+            students.add(cell.getRowIndex() - 1, student);
+        }
+        return students;
+    }
+
+    public static List<University> readUniversities(String path) throws IOException {
+        List<University> universities = new ArrayList<>();
+        File file = new File(path);
+        FileInputStream fis = new FileInputStream(file);
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+        XSSFSheet sheet = wb.getSheetAt(1);
+        Iterator<Row> itr = sheet.iterator();
+        itr.next();
+        while (itr.hasNext()) {
+            Row row = itr.next();
+            Iterator<Cell> cellIterator = row.cellIterator();
+            University university = new University();
+            Cell cell = null;
+            while (cellIterator.hasNext()) {
+                cell = cellIterator.next();
+                if (cell.getCellType() == CellType.STRING) {
+                    if (cell.getColumnIndex() == 0) {
+                        university.setId(cell.getStringCellValue());
+                    } else if (cell.getColumnIndex() == 1) {
+                        university.setFullName(cell.getStringCellValue());
+                    } else if (cell.getColumnIndex() == 2) {
+                        university.setShortName(cell.getStringCellValue());
+                    }
+                } else if (cell.getCellType() == CellType.NUMERIC && cell.getColumnIndex() == 3) {
+                    university.setYearOfFoundation((int) cell.getNumericCellValue());
+                } else if (cell.getColumnIndex() == 4) {
+                    university.setMainProfile(StudyProfile.valueOf(cell.getStringCellValue()));
+                }
+            }
+            universities.add(cell.getRowIndex() - 1, university);
+        }
+        return universities;
+    }
+}
